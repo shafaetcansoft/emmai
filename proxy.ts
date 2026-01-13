ff25895d85d9ebd8c256e5a9d3e15e18843e81b1
Syed Shafaet Hossain <shafaet@cansoft.com>
2026-01-13 21:42:10 +0600

    middleware dprecated fixed

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { env } from './env';
 
-export function middleware(req: NextRequest) {
+export function proxy(req: NextRequest) {
   const host = req.headers.get('host');
   if (!host) return NextResponse.next();
 
